b8f7173124e9599184cbadd9e0f8f791678d2c5e
francososa97 <franco.gonzalez.sosa@gmail.com>
2021-04-17 23:02:46 +0200

    feat: comit inicial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,88 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
+const  App = () =>  {
+
+
+  const SetHorarios = () =>{
+    let horarios=[];
+
+    for (let hora = 0; hora < 24; hora++) {
+      let horario="";
+      for (let minutos = 0; minutos < 2; minutos++) {
+        if(hora >= 8 && hora < 20)
+        {
+          horario = `${hora}:${minutos === 0 ? "00" : "30"}-${minutos===0? `${hora}:30`: `${hora+1}:00`}`;
+          horarios = [...horarios,horario];
+        }
+      }
+      
+    }
+    let horariosDisponibles = horarios.filter(horario => horario !== "");
+    console.log(horariosDisponibles);
+    return horariosDisponibles;
+  }
+  
+  const [motos,SetMotos] = useState(8);
+  const [horarios,SetHorariosDisponibles]=useState(SetHorarios());
+
+  const ReservarHorario = (indice,horarioSolicitado) => {
+
+
+    let horarioReservado = horarioSolicitado.includes("no disponible");
+    if(horarioReservado)
+    {
+      SetMotos(motos + 1);
+      let horariosActuales = [...horarios];
+      let horariosReserva = SetHorarios();
+      let horarioOriginal = horariosReserva[indice];
+      horariosActuales[indice] = horarioOriginal;
+      SetHorariosDisponibles(horariosActuales);
+    }
+    else
+    {
+      if(motos > 0){
+        SetMotos(motos - 1 );
+        SetHorariosDisponibles(horarios.map(horario => horario === horarioSolicitado ? "no disponible" : horario ));
+      }
+    }
+
+
+  }
+
+  const EstaDisponible = (horario) =>{
+
+    let horarioReservado = horario.includes("no disponible");
+    let respuesta = horarioReservado ? "celda-reservada": "celda-disponible";
+
+    return respuesta;
+
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>
+          {motos > 0 ? ` Cantidad motos: ${motos}` : `No hay mas horarios disponibles...`}
+        </h4>
+        <table class="table">
+          <thead className="thead-dark">
+            <tr>
+              <th className="titulo" scope="col">Horario</th>
+            </tr>
+          </thead>
+          <tbody>
+            {horarios.map((horario,i)=>
+              {
+                return(
+                  <tr>
+                    <th onClick={() =>ReservarHorario(i,horario)}  className={EstaDisponible(horario)} scope="row">{horario}</th>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+
       </header>
     </div>
   );
